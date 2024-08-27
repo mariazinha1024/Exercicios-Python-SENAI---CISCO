e7871e098a2fb9fd447ca2449d02d372b6fdf5ad
Maria Eduarda de Menezes <111156804+mariazinha1024@users.noreply.github.com>
2024-08-27 16:18:42 -0300

    ex3.py

--- Exercícios Operadores Lógicos/ex3.py
+++ Exercícios Operadores Lógicos/ex3.py
@@ -2,6 +2,11 @@
 
 algo = input("Digite algo: ")
 print("As classe do que você digitou é: ")
-print(type(algo))
-print(id(algo))
+print(f"A variável é do tipo {type(algo)}.")
+print(f"A variável é do tipo numérico? {algo.isnumeric()}.")
+print(f"A variável é do tipo string? {algo.isalpha()}.")
+print(f"A variável é do tipo alfanumérico? {algo.isalnum()}")
+print(f"A variável é do tipo maiusculo? {algo.isupper()}")
+print(f"A variável é do tipo minusculo? {algo.islower()}")
+
 print(f"O que você digitou foi: {algo}.")
