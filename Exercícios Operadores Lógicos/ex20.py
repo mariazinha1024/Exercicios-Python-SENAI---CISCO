39fc86001c09e02270ae155db97772994547d313
Maria Eduarda de Menezes <111156804+mariazinha1024@users.noreply.github.com>
2024-08-27 16:30:06 -0300

    ex20.py

--- Exercícios Operadores Lógicos/ex20.py
+++ Exercícios Operadores Lógicos/ex20.py
@@ -1,12 +1,15 @@
-#20) Escreva um programa que receba o salário bruto de um funcionário, bem como os percentuais de descontos de INSS, Imposto de Renda, e um valor de bonificação. O funcionário recebe 30% do valor do salário bruto no dia 
+#20) Escreva um programa que receba o salário bruto de um funcionário, bem como os percentuais de descontos de INSS, Imposto de Renda, e um valor de bonificação. O funcionário recebe 30% do valor do salário bruto no dia
 # 15 e o restante em um segundo pagamento no final do mês, dia 30. O programa deve calcular e exibir o salário final do funcionário após aplicar os descontos e a bonificação.
 
 salario = float(input("Qual o seu salário bruto? "))
-inss = float(input("Quanto é descontado de INSS do seu salário? "))
-ir = float(input("Quanto é descontado de Imposto de Renda do seu salário? "))
+inss = float(input("Quanto é o % descontado de INSS do seu salário? "))
+ir = float(input("Quanto é o % descontado de Imposto de Renda do seu salário? "))
 bonificacao = float(input("Quanto de bonificação você recebe? "))
 
-total = salario-inss-ir+bonificacao
+inssper = salario * inss/100
+irper = salario*ir/100
+
+total = salario-inssper-irper+bonificacao
 salario1 = total*30/100
 salario2 = total-salario1
 print(f"Você receberá {total:.2f} reais, sendo {salario1:.2f} reais no dia 15 e {salario2:.2f} reais no dia 30.")
