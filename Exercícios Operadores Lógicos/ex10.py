df09f426a7ac067473c141baefd77b3c08f1f91c
Maria Eduarda de Menezes <111156804+mariazinha1024@users.noreply.github.com>
2024-08-27 16:22:43 -0300

    ex10.py

--- Exercícios Operadores Lógicos/ex10.py
+++ Exercícios Operadores Lógicos/ex10.py
@@ -1,8 +1,13 @@
 # 10) Uma fábrica de refrigerantes produz garrafas de 350ml, 600ml e 2 litros. Crie um programa que peça ao usuário a quantidade produzida de cada tamanho e calcule o volume total de refrigerante produzido em litros.
 
-pequena = int(input("Quantos litros foram produzidos para as garradas de 350ml?"))
-media = int(input("Quantos litros foram produzidos para as garradas de 600ml?"))
-grande = int(input("Quantos litros foram produzidos para as garradas de 2l?"))
+pequena = int(input("Quantas garrafas de 350ml foram produzidas?"))
+media = int(input("Quantas garrafas de 600ml foram produzidas?"))
+grande = int(input("Quantas garrafas de 2l foram produzidas?"))
 
-total = pequena + media + grande
-print(f"o volume total produzido foi de: {total} litros.")
+qntpqn = (pequena*350)/1000
+qntmedia = (media*600)/1000
+qntgrande = grande*2
+
+total = qntpqn+qntgrande+qntmedia
+
+print(f"O volume total produzido foi de: {total} litros.")
